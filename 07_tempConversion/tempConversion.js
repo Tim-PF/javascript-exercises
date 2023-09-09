57e5788efdb2b5359ea3261187c56903d2475404
Tim-PF <tim.pfops@gmail.com>
2023-09-09 14:03:11 +0200

    Write code so test will pass

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,23 @@
-const convertToCelsius = function() {
+//C = 5/9 x (F-32)
+const convertToCelsius = function(temperature) {
+  let celsius = 5/9 * (temperature -32)
+  if (Number.isInteger(celsius)) {
+    return celsius;
+  }
+  else {
+  return parseFloat(celsius.toFixed(1));
+  }
 };
-
-const convertToFahrenheit = function() {
+//°F = °C × (9/5) + 32
+const convertToFahrenheit = function(temperature) {
+  let fahrenheit = temperature * (9/5) + 32
+  if (Number.isInteger(fahrenheit)) {
+    return fahrenheit;
+  }
+  else {
+    return parseFloat(fahrenheit.toFixed(1));
+  }
+  
 };
 
 // Do not edit below this line
