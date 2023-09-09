aa13bab30e982a643bd8b79a38f449866727a977
Tim-PF <tim.pfops@gmail.com>
2023-09-09 11:27:57 +0200

    Finished tests to sumALL 2 values

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,28 @@
-const sumAll = function() {
+const sumAll = function(value1 , value2) {
+    let min , max;
+    if (value1 < 0 || value2 <0) {
+        return 'ERROR';
+    
+    }
+    if (typeof(value1) !== 'number' || typeof(value2) !== 'number' ) {
+        return 'ERROR';
+    }
+    if (value1 < value2) {
+        min = value1;
+        max = value2;
+    }
+    else {
+         min = value2;
+         max = value1;
+    }
+
+    let sum = 0;
+
+    for (let i = min; i <= max; i++) {
+       sum += i 
+    }
+    return sum;
+
 
 };
 
