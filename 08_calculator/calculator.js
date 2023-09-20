5f3fd27da50d72afc71d70807c9f717a943e651e
Tim-PF <tim.pfops@gmail.com>
2023-09-20 12:14:55 +0200

    Solved all problems using reduce or simple math

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
+const add = function (a, b) {
+  return a + b;
 	
 };
 
-const subtract = function() {
+const subtract = function(a, b) {
+	return a - b
+};
+
+const sum = function(array) {
+  return array.reduce((total, items) => {
+  return total + items;
+  }, 0);
+
+};
+
+const multiply = function(array) {
+  return array.reduce((product, items) => product * items);
+
+};
+
+const power = function(a,b) {
+  return Math.pow(a,b);
 	
 };
 
-const sum = function() {
-	
-};
+const factorial = function (n) {
+  if (n === 0) return 1;
+  let product = 1;
+  for (let i = n; i > 0; i--) {
+    product *= i;
+  }
+  return product;
 
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
 };
 
 // Do not edit below this line
