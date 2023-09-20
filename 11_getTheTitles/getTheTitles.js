c7fcb580fa60509733fd44e8c60b5a3f57c0bd0d
Tim-PF <tim.pfops@gmail.com>
2023-09-20 15:27:17 +0200

    Used reduce to find the oldest person

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    let newArray = [];
+ array.forEach(title => {
+    
+    current = title.title;
+    newArray.push(current)
+})
+return newArray;
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    return array.reduce((oldest , current) => {
+       const oldestAge = calcAge(oldest.yearOfBirth, oldest.yearOfDeath);
+       const currentAge = calcAge(current.yearOfBirth, current.yearOfDeath);
+    
+       return oldestAge < currentAge ? current : oldest;
+    });
 };
+
+const calcAge = function (birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
