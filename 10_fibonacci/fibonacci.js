0fd5fb3561385bdfdb41592a302356e76b6ae758
Tim-PF <tim.pfops@gmail.com>
2023-09-20 14:21:07 +0200

    Solved fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (n < 0) {
+        return "OOPS"
+    }
+   let myArray = [];
+   newNum = 1;
+   oldNum = 0;
+   for (let i = 0; i<n; i++) {
+    
+    myArray.push(newNum)
+    newNum += oldNum;
+    oldNum = myArray[i]
+   }
+   return myArray[n-1];
 
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
